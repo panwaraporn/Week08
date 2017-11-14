af4b83dfcaceffe919b2ace55dd1b62881a04b74
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 04:08:16 +0700

    commit Lab07P04-2

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
+
 namespace ConsoleAppArray
 {
-class Program
+    class Program
     {
         static void Main(string[] args)
         {
             int[] data = new int[10];
-            int p = 0;
-            for (int i = 0; i < data.Length; i++) { data[i] = i; }
-            Console.WriteLine("data[7] : " + data[7]);
-            Console.WriteLine("data[4] + data[9] : " + (data[4] + data[9]));
-            Console.WriteLine("data[2] == data[6] ? : "
-                + (data[2]==data[6] ? "eq" : "n eq"));
-            for (int i = 0; i < data.Length; i++)
-            { p += data[i]; }
-            Console.WriteLine("Sum : " + p);
-
-            Console.Write("All values : ");
-            for (int i = 0; i < data.Length; i++)
-            { Console.Write(data[i] + " "); }
+            Student[] student = new Student[31]; // สร้างอาร์เรย์ของวัตถุจากคลาส
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
             Console.ReadLine();
+        }
+        class Student     // nested class
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
         }
     }
 }
