bd99c2d6bfba12568affba46029d5ca90e92c132
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 04:24:02 +0700

    commit Lab07P08-2

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -1,30 +1,52 @@
 ﻿using System;
+using System.Linq;
+
 namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[] GPS = new double[8];
-            double GPA = 0;
-            for (int i = 0; i < GPS.Length; i++)
-                
-        {
-                Console.Write("Grade on semester {0} : ", i + 1);
-                GPS[i] = double.Parse(Console.ReadLine());
-            }
-            Console.WriteLine(">>>>>>>>>>>>");
-            for (int i = 0; i < GPS.Length; i++)
-            {
-                Console.WriteLine("Grade on semester {0} : {1}", i + 1, GPS[i]);
-            }
-            Console.WriteLine(">>>>>>>>>>>>");
-            for (int i = 0; i < GPS.Length; i++)
-            {
-                GPA += GPS[i];
-            }
-            Console.WriteLine("GPA is : " + GPA / GPS.Length);
-            Console.ReadKey();
+            // Array of characters.
+            char[] array1 = { 'q', 'w', 'e', 'r', 't',
+                'y', 'u', 'i', 'o', 'p' };
+            Array.Sort(array1);
+
+            // Array of strings.
+            string[] colors = new string[] { "red",
+              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
+            Array.Sort(colors);
+
+            string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
+              "Indonesia", "Singapore", "Thailand", "Philippines",
+              "Vietnam", "Brunei Darussalam", "Laos", "Myanmar" };
+
+            // C# program that uses LINQ
+            var sortAscending = from country in AseanCountries
+                                orderby country
+                                select country;
+            var sortDescending = from country in AseanCountries
+                                 orderby country descending
+                                 select country;
+
+            // print output
+            Console.WriteLine("------Character sorting----------");
+            foreach (var c in array1)
+                Console.WriteLine(c);
+
+            Console.WriteLine("------String sorting----------");
+            foreach (string color in colors)
+                Console.WriteLine(color);
+
+            Console.WriteLine("------String sort ascending----------");
+            foreach (string c in sortAscending)
+                Console.WriteLine(c);
+
+            Console.WriteLine("------String sort descending----------");
+            foreach (string c in sortDescending)
+                Console.WriteLine(c);
+            // wait
+            Console.ReadLine();
         }
     }
 }
