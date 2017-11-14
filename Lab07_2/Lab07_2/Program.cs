8a0843d2d133620877b08f95e6721237d43a66c3
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 04:14:39 +0700

    commit Lab07P06-2

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
-
 namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
-            Console.WriteLine("Length of data = {0}", data.Length);
-            for (int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine(data[i]);
+            double[] GPS = new double[8];
+            double GPA = 0;
+            for (int i = 0; i < GPS.Length; i++)
+                
+        {
+                Console.Write("Grade on semester {0} : ", i + 1);
+                GPS[i] = double.Parse(Console.ReadLine());
             }
-            Console.ReadLine();
+            Console.WriteLine(">>>>>>>>>>>>");
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                Console.WriteLine("Grade on semester {0} : {1}", i + 1, GPS[i]);
+            }
+            Console.WriteLine(">>>>>>>>>>>>");
+            for (int i = 0; i < GPS.Length; i++)
+            {
+                GPA += GPS[i];
+            }
+            Console.WriteLine("GPA is : " + GPA / GPS.Length);
+            Console.ReadKey();
         }
     }
 }
