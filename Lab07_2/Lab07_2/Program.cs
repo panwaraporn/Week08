6094051f148529e2ad3ca28439cc292c8e1e0be8
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 16:11:30 +0700

    commit Lab09P09-3

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -10,47 +10,35 @@ namespace ConsoleAppArray
             int[] ArrayB = new int[6];
             // copy by operator =
             ArrayB = ArrayA;
-
             Console.WriteLine("*** Array copy by operator = ***");
             Console.WriteLine("===== Before =====");
             for (int i = 0; i < ArrayA.Length; i++)
             {
-                Console.WriteLine("arrayA[{0}] = {1}, 
-                  ArrayB[{ 0}] = { 2}
-                ",i, ArrayA[i], ArrayB[i]);
+                Console.WriteLine("arrayA[{0}] = {1},ArrayB[{ 0}] = { 2}" ,i, ArrayA[i], ArrayB[i]);
             }
             // change element 0 of ArrayA
             ArrayA[0] = 9;
             Console.WriteLine("===== After ======");
             for (int i = 0; i < ArrayA.Length; i++)
             {
-                Console.WriteLine("ArrayA[{0}] = {1}, 
-                  ArrayB[{ 0}] = { 2}
-                ", i, ArrayA[i], ArrayB[i]);
+                Console.WriteLine("ArrayA[{0}] = {1},ArrayB[{ 0}] = { 2}" , i, ArrayA[i], ArrayB[i]);
             }
-
             // copy by method Array.Copy()
             int[] ArrayC = new int[6];
             Array.Copy(ArrayA, ArrayC, ArrayA.Length);
-
             Console.WriteLine("*** Array copy by method Array.Copy() ***");
             Console.WriteLine("===== Before =====");
             for (int i = 0; i < ArrayA.Length; i++)
             {
-                Console.WriteLine("ArrayA[{0}] = {1}, 
-                  ArrayC[{ 0}] = { 2}
-                ", i, ArrayA[i], ArrayC[i]);
+                Console.WriteLine("ArrayA[{0}] = {1},ArrayC[{ 0}] = { 2}" , i, ArrayA[i], ArrayC[i]);
             }
             // change element 0 of ArrayA
             ArrayA[0] = 1;
             Console.WriteLine("===== After =====");
             for (int i = 0; i < ArrayA.Length; i++)
             {
-                Console.WriteLine("ArrayA[{0}] = {1}, 
-                  ArrayC[{ 0}] = { 2}
-                ", i, ArrayA[i], ArrayC[i]);
+                Console.WriteLine("ArrayA[{0}] = {1}, ArrayC[{ 0}] = { 2}" , i, ArrayA[i], ArrayC[i]);
             }
-
             // wait
             Console.ReadLine();
         }
