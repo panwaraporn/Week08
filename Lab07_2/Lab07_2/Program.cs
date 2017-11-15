b5a2b3415fba838d5d3603252bd035ac6514cb89
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 16:21:20 +0700

    commit Lab07P10-2

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -1,46 +1,37 @@
 ﻿using System;
-
 namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] ArrayA = new int[] { 1, 2, 3, 4, 5, 6 };
-            int[] ArrayB = new int[6];
-            // copy by operator =
-            ArrayB = ArrayA;
-            Console.WriteLine("*** Array copy by operator = ***");
-            Console.WriteLine("===== Before =====");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("arrayA[{0}] = {1},ArrayB[{ 0}] = { 2}" ,i, ArrayA[i], ArrayB[i]);
-            }
-            // change element 0 of ArrayA
-            ArrayA[0] = 9;
-            Console.WriteLine("===== After ======");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[{0}] = {1},ArrayB[{ 0}] = { 2}" , i, ArrayA[i], ArrayB[i]);
-            }
-            // copy by method Array.Copy()
-            int[] ArrayC = new int[6];
-            Array.Copy(ArrayA, ArrayC, ArrayA.Length);
-            Console.WriteLine("*** Array copy by method Array.Copy() ***");
-            Console.WriteLine("===== Before =====");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[{0}] = {1},ArrayC[{ 0}] = { 2}" , i, ArrayA[i], ArrayC[i]);
-            }
-            // change element 0 of ArrayA
-            ArrayA[0] = 1;
-            Console.WriteLine("===== After =====");
-            for (int i = 0; i < ArrayA.Length; i++)
-            {
-                Console.WriteLine("ArrayA[{0}] = {1}, ArrayC[{ 0}] = { 2}" , i, ArrayA[i], ArrayC[i]);
-            }
-            // wait
-            Console.ReadLine();
+            string[] months = new string[12];
+            months[0] = "January";
+            months[1] = "February";
+            months[2] = "March";
+            months[3] = "April";
+         
+            months[4] = "May";
+            months[5] = "June";
+            months[6] = "July";
+            months[7] = "August";
+            months[8] = "September";
+            months[9] = "October";
+            months[10] = "November";
+            months[11] = "December";
+
+            string value1 = Array.Find(months,
+            element => element.Length == 3);
+            // b) ค้นหาชื่อเดือนแรกที่มีความยาวมากกว่า 10 ตัวอักษร
+            string value2 = Array.Find(months,
+            element => element.Length > 10);
+            // c) ค้นหาชื่อเดือนที่มีตัวอักษรตัวแรกเหมือนเดือนมกราคม แ ล้วนำมาแสดงรวมกันในบรรทัดเดียว คั่นด้วย,
+            string[] array1 = Array.FindAll(months,
+            element => element.StartsWith("J", StringComparison.Ordinal));
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            Console.WriteLine(string.Join(",", array1));
+            Console.ReadKey();
         }
     }
 }
