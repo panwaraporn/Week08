3700c96cdd4c7c31b1ccee7f16f37efd29189389
panwaraporn <58030219@kmitl.ac.th>
2017-11-15 17:05:16 +0700

    commit Lab07P16-2

--- Lab07_2/Lab07_2/Program.cs
+++ Lab07_2/Lab07_2/Program.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MultidimensionalArray
+namespace ConsoleAppArray
 {
     class Program
     {
         static void Main(string[] args)
         {
             int[,] multiplyTable = new int[12, 12];
-            multiplyTable[0, 0] = 2 * 1;
-            multiplyTable[1, 0] = 2 * 2;
-            multiplyTable[2, 0] = 2 * 3;
-            multiplyTable[3, 0] = 2 * 4;
-            multiplyTable[4, 0] = 2 * 5;
-            multiplyTable[5, 0] = 2 * 6;
-            multiplyTable[6, 0] = 2 * 7;
-            multiplyTable[7, 0] = 2 * 8;
-            multiplyTable[8, 0] = 2 * 9;
-            multiplyTable[9, 0] = 2 * 10;
-            multiplyTable[10, 0] = 2 * 11;
-            multiplyTable[11, 0] = 2 * 12;
+            for (int i = 0, table = 2; i < multiplyTable.GetLength(0); i++, table++)
+            {
+                for (int j = 0; j < multiplyTable.GetLength(1); j++)
+                {
+                    multiplyTable[j, i] = table * (j + 1);
+                }
+            }
             for (int row = 0; row < multiplyTable.GetLength(0); row++)
             {
                 for (int col = 0; col < multiplyTable.GetLength(1); col++)
@@ -31,7 +21,8 @@ namespace MultidimensionalArray
                 }
                 Console.WriteLine();
             }
-            Console.ReadLine();
+            52
+        Console.ReadLine();
         }
     }
 }
